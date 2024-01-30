d9d19dad7b9a47a5dab4e391ae7fc2bd85cce29b
SiddharthVPillai <68557526+SiddharthVPillai@users.noreply.github.com>
2024-01-30 10:19:03 +0530

    Update HelloWorld.py
    12

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,6 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo...Jan 2024...\n")
 print("Hello World Demo...Jan 2024...\n")
-print("Hello World Demo...Jan 2024...\n")
 
 
 print("Hemlo")
